e3ae44c553b2b1feb68b486ef534bde3e0579724
amitasulin <amitasulin7@gmail.com>
2023-11-12 23:45:55 +0200

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+dotenv.config();
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("successfully connected to database");
+  } catch (error) {
+    console.log("Error connecting to database:", error);
+  }
+};
+
+connectDatabase();
+
+app.listen(8800, () => {
+  console.log("Backend server is running");
+});
